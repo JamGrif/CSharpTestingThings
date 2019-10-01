5eb30ab3fdbaf54cd0d4652ae932a87d2085ef4d
JamGrif <40271350+JamGrif@users.noreply.github.com>
2019-10-01 20:50:35 +0100

    Main

---
+++ CSharp Fundamentals/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstName = "";
+            string LastName = "";
+            Console.WriteLine("Hello World!");
+            Console.Write("Enter your first name: ");
+            FirstName = Console.ReadLine();
+            Console.Write("Enter your last name: ");
+            LastName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + FirstName + " " + LastName);
+
+            string password = "apple";
+            string attempt = "";
+            while (password != attempt)
+            {
+                Console.Write("Enter the password: ");
+                attempt = Console.ReadLine();
+                if (attempt == password)
+                {
+                    break;
+                }
+                Console.WriteLine("Wrong password!");
+            }
+            Console.WriteLine("Password accepted!");
+
+
+        }
+    }
+}
